054e24782d68b255b7a4cd6feda23bc33735bcf6
Juan_Hoyos <juanandreshoyosocampo@gmail.com>
2025-08-28 14:21:09 -0500

    Código terminado con sus respectivas pistas de tipo y verificado por test_model_unittest.py.

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 class Note:
     def __init__(self, text: str, page: int, date: datetime):
         self.text: str = text
@@ -30,7 +29,7 @@ class Book:
         return True
 
     def set_rating(self, rating: int) -> bool:
-        if rating != Book.EXCELLENT or rating != Book.GOOD or rating != Book.BAD:
+        if rating != Book.EXCELLENT and rating != Book.GOOD and rating != Book.BAD:
             return False
         self.rating = rating
         return True
@@ -45,7 +44,7 @@ class Book:
     def page_with_most_notes(self) -> int:
         comparar: dict[int, int] = {}
         contador: int = 0
-        pagina = 0
+        pagina: int = 0
 
         if len(self.notes) == 0:
             return -1
@@ -55,14 +54,68 @@ class Book:
                 comparar[note.page] += 1
             else:
                 comparar[note.page] = 1
-        for clave, cont in comparar.values():
+        for clave, cont in comparar.items():
             if cont > contador:
                 contador = cont
                 pagina = clave
 
         return pagina
 
+    def __str__(self) -> str:
+        if self.rating == Book.EXCELLENT:
+            rating = "excellent"
+        if self.rating == Book.GOOD:
+            rating = "good"
+        if self.rating == Book.BAD:
+            rating = "bad"
+        else:
+            rating = "unrated"
+        return f"ISBN: {self.isbn}\nTitle: {self.title}\nAuthor: {self.author}\nPages: {self.pages}\nRating: {rating}"
 
+class ReadingDiary:
+
+    def __init__(self):
+        self.books: dict[str,Book] = {}
+
+    def add_book(self, isbn: str, title: str, author: str, pages: int) -> bool:
+        if isbn  in self.books:
+            return False
+        else:
+            self.books[isbn] = Book(isbn, title, author,pages)
+            return True
+
+    def search_by_isbn(self, isbn: str) -> Book|None:
+        for clave,valor in self.books.items():
+            if isbn == clave:
+                return valor
+        return None
+
+    def add_note_to_book(self,isbn: str, text: str, page: int, date: datetime) -> bool:
+        book: Book = self.search_by_isbn(isbn)
+        if book is None:
+            return False
+        else:
+            return book.add_note(text,page,date)
+
+    def rate_book(self, isbn: str, rating: int) -> bool:
+        book: Book|None = self.search_by_isbn(isbn)
+        if book is None:
+            return False
+        return book.set_rating(rating)
+
+    def book_with_most_notes(self) -> Book|None:
+        mayor: int = 0
+        libro: Book|None = None
+
+        if len(self.books) == 0:
+            return None
+
+        for book in self.books.values():
+            if len(book.notes) > mayor:
+                mayor = len(book.notes)
+                libro = book
+
+        return libro
 
 
 
